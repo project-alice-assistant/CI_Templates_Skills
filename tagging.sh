bc0d0a04892ddf31fce01db3417feee54bd50901
maxbachmann <ssh@maxbachmann.de>
2020-01-17 23:08:29 +0100

    add tagging

---
+++ tagging.sh
@@ -0,0 +1,12 @@
+version=$(jq -r .version *.install)
+aliceMinVersion=$(jq -r .aliceMinVersion *.install)
+git_refs_url="https://api.github.com/repos/project-alice-assistant/$CI_PROJECT_NAME/git/refs"
+new_tag="$version>=$aliceMinVersion"
+curl -s -X POST $git_refs_url \
+-H "Authorization: token $GITHUB_TOKEN" \
+-d @- << EOF
+{
+  "ref": "refs/tags/$new_tag",
+  "sha": "$CI_COMMIT_SHA"
+}
+EOF
