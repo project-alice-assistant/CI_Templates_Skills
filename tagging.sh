ecad1bf50960cf876b93bf59714e30d25f387cf6
Psycho <laurentchervet@bluewin.ch>
2020-02-16 12:44:30 +0100

    Update tagging.sh
    Adding echo

--- tagging.sh
+++ tagging.sh
@@ -2,6 +2,7 @@ version=$(jq -r .version *.install)
 aliceMinVersion=$(jq -r .aliceMinVersion *.install)
 git_refs_url="https://api.github.com/repos/project-alice-assistant/$CI_PROJECT_NAME/git/refs"
 new_tag="$version_$aliceMinVersion"
+echo "Trying to push new tag $new_tag"
 curl -s -X POST $git_refs_url \
 -H "Authorization: token $GITHUB_TOKEN" \
 -d @- << EOF
